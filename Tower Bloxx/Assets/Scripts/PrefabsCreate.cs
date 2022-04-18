91da0b580ecf890c8059631815426039c8ed7c6b
Matias <matiasmonti2011@hotmail.com>
2022-04-18 13:13:06 -0300

    Add commentary

--- Tower Bloxx/Assets/Scripts/PrefabsCreate.cs
+++ Tower Bloxx/Assets/Scripts/PrefabsCreate.cs
@@ -5,6 +5,7 @@ public class PrefabsCreate : MonoBehaviour
 {
     [SerializeField] private GameObject cubes;
     [SerializeField] private List<GameObject> cubos;
+    [SerializeField] private GameObject claw;
     [SerializeField] private GameObject crane;
 
     private int spawnPosY = 1;
@@ -18,9 +19,18 @@ public class PrefabsCreate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Condicion para generar solo el cubo
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            cubos.Add(Instantiate(cubes, new Vector3(0, crane.transform.position.y - spawnPosY, 0), Quaternion.identity));
+            cubos.Add(Instantiate(cubes, new Vector3(0, claw.transform.position.y - spawnPosY, 0), Quaternion.identity));
+
+            //Mover crane cada vez que se tira el cubo
+            if (true)
+            {
+                crane.transform.position = new Vector3(0, crane.transform.position.y + spawnPosY, 0);
+            }
         }
+
+        //Mover crane para los costados
     }
 }
